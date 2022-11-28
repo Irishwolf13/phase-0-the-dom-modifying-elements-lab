685bf45a24ec45fb1018987b7cfbc031ce4ba1ed
John Rooney <rooneyjohn@gmail.com>
2022-11-28 10:12:02 -0500

    commit tree e8e27d2d8ea5c4a6127487bf3a5cf547b087f68a

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const myElement = document.getElementById("main");
+myElement.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Rooeny is the champion";
+document.body.append(newHeader);
